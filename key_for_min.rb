032645517346354fd9655d0501799a18d49cc76d
Michael Naraiswar <naraiswar@gmail.com>
2019-03-07 02:12:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,12 +6,12 @@ def key_for_min_value(name_hash)
     nil
   else
     number = 0
-    keyName = "" 
+    keyName = ""
     name_hash.each do |key, value|
       if number == 0
         number = value
         keyName = key
-      else 
+      else
         if value < number
           number = value
           keyName = Key
